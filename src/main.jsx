add016d0193e49895b90949a1d77783195eac85e
Mark Anderson <markanderson@Mark-Andersons-Grafana-MacBook-Pro.local>
2026-04-05 08:54:06 -0600

    Config added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ATDevelopmentTracker from './ATDevelopmentTracker'
 import ClinicFeedback from './ClinicFeedback'
+import { SHEETS_API_URL } from './config'
 
-// Bridge Vite environment variable to window global for ATDevelopmentTracker
-window.__AT_API_URL__ = import.meta.env.VITE_SHEETS_API_URL || "";
+// Bridge API URL to window global for ATDevelopmentTracker
+window.__AT_API_URL__ = SHEETS_API_URL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
